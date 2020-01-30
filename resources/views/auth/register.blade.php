e72ae6e518c27b5c64770c76c7c4c25e4d5854c2
Romanti-Ezer Santos <romanti.santos@fatec.sp.gov.br>
2020-01-30 14:55:39 -0300

    Login, register and forgot password views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="bg-gray-300 mx-auto h-full flex justify-center items-center">
+        <div class="w-96 p-6 rounded-lg bg-blue-900 shadow-xl">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+            <svg class="w-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 187.14746 61.64453"><title>Ativo 5</title><g id="Camada_2" data-name="Camada 2"><g id="Camada_1-2" data-name="Camada 1"><path d="M46.398,52.479a5.30156,5.30156,0,0,1-5.126,4.14063c-2.49756,0-4.40332-1.90625-4.0747-4.14063l3.15478-22.60742L30.82227,44.59229a5.50607,5.50607,0,0,1-4.73145,2.498,3.80286,3.80286,0,0,1-3.87744-2.498L16.7583,29.87158,13.604,52.479c-.32861,2.23438-2.76025,4.14063-5.32324,4.14063A3.65791,3.65791,0,0,1,4.40332,52.479L9.85791,13.44141A5.292,5.292,0,0,1,15.24707,9.104c2.103,0,3.61426.8545,4.20606,2.563l8.47753,23.19873L42.84912,11.60156A5.53192,5.53192,0,0,1,47.77832,9.104a3.80081,3.80081,0,0,1,4.07422,4.33741Z" transform="translate(-4.36288 -9.104)" style="fill:#fff"/><path d="M64.603,44.00146c-.39405,3.02247.59179,4.92872,3.812,4.92872a9.05459,9.05459,0,0,0,5.58594-1.709L76.9585,26.45361c.26269-2.23437,2.03711-3.94238,5.06054-3.94238,3.02295,0,4.27149,1.708,4.00879,3.94238L81.55908,58.19678c-.98584,7.03222-5.915,12.55176-15.51025,12.55176-3.61426,0-10.3833-1.18262-10.3833-4.92872,0-2.2998,2.16845-4.13964,4.73144-4.13964a5.94489,5.94489,0,0,1,2.0376.39355,8.33882,8.33882,0,0,0,4.14014,1.11719c3.74609,0,5.45508-1.83985,5.915-4.99414l.394-2.82617A19.42069,19.42069,0,0,1,66.312,56.42236c-9.46387,0-11.76367-5.32324-10.77783-12.4209l2.49707-17.54785c.2627-2.23437,2.169-3.94238,5.06055-3.94238s4.3374,1.708,4.00879,3.94238Z" transform="translate(-4.36288 -9.104)" style="fill:#fff"/><path d="M115.209,54.31885,103.64209,27.76807l-3.41748,24.5791a5.44036,5.44036,0,0,1-5.25732,4.27246,3.74616,3.74616,0,0,1-3.94336-4.27246l5.45459-38.77442A5.29341,5.29341,0,0,1,101.86768,9.104a4.651,4.651,0,0,1,4.14062,2.43164c4.46875,10.1211,6.96582,16.10157,11.50049,26.68262l3.4834-24.84228a5.44044,5.44044,0,0,1,5.25732-4.272,3.74577,3.74577,0,0,1,3.94336,4.272L124.73828,52.1499a5.37865,5.37865,0,0,1-5.45508,4.46973A4.56163,4.56163,0,0,1,115.209,54.31885Z" transform="translate(-4.36288 -9.104)" style="fill:#fff"/><path d="M141.95947,43.08154l-.32861,2.168c-.32861,2.23438,1.1831,3.68067,4.73193,3.68067a15.62065,15.62065,0,0,0,6.17774-1.44629,4.20641,4.20641,0,0,1,1.97168-.39356c2.82568,0,3.61425,1.77442,3.61425,3.1543,0,4.99414-9.06933,6.24316-13.47216,6.24316-8.6753,0-12.94678-4.99414-12.02686-11.501l1.51172-10.97461c.91992-6.6377,6.50586-11.69825,15.64111-11.69825,7.75489,0,11.501,3.877,11.501,9.59473,0,7.09766-4.272,11.56641-14.32715,11.56641A33.29445,33.29445,0,0,1,141.95947,43.08154Zm6.769-13.73633c-3.1543,0-5.06006,1.64356-5.45459,4.27247l-.394,2.82617c2.103.19629,4.07422.26269,4.73145.26269,2.82617,0,4.60058-1.05175,4.60058-4.40332C152.21191,30.19971,151.02881,29.34521,148.72852,29.34521Z" transform="translate(-4.36288 -9.104)" style="fill:#fff"/><path d="M169.49707,16.85889a4.76651,4.76651,0,0,1,5.06055-4.00879c2.8916,0,4.3374,1.83984,4.00879,4.00879l-1.05176,7.29492h4.86328a3.55116,3.55116,0,0,1,3.812,3.4834c0,2.49707-2.30029,4.33691-4.9292,4.33691h-4.86328L175.01758,41.898c-.65723,4.86328-.52588,6.57226,2.6289,6.57226.59131,0,.98536-.06542,1.11719-.06542,3.68018,0,4.92871.91992,4.92871,2.8916,0,4.40332-3.35156,4.99414-6.96631,4.99414-10.18652,0-12.09228-4.99414-10.84375-13.93262Z" transform="translate(-4.36288 -9.104)" style="fill:#fff"/><rect x="90.74319" y="52.17004" width="96.40427" height="2.53695" style="fill:#fff"/></g></g></svg>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+            <h1 class="text-white text-3xl pt-6">Join Us</h1>
+            <h2 class="text-base text-blue-300">Enter your information below</h2>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <form method="POST" action="{{ route('register') }}" class="pt-8">
+                @csrf
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                <div class="relative">
+                    <label for="name" class="text-blue-500 text-xs font-bold pl-3 pt-2 absolute uppercase">Full Name</label>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <input id="name" type="text" class="w-full p-3 pt-8 rounded focus:outline-none focus:bg-blue-700 bg-blue-800 text-gray-100 @error('name') border border-red-600 @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Your Name">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    @error('name')
+                    <span class="text-red-600 text-xs" role="alert"><strong>{{ $message }}</strong></span>
+                    @enderror
                 </div>
-            </div>
+
+                <div class="relative pt-3">
+                    <label for="email" class="text-blue-500 text-xs font-bold pl-3 pt-2 absolute uppercase">E-Mail</label>
+
+                    <input id="email" type="email" class="w-full p-3 pt-8 rounded focus:outline-none focus:bg-blue-700 bg-blue-800 text-gray-100 @error('email') border border-red-600 @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="your@email.com">
+
+                    @error('email')
+                    <span class="text-red-600 text-xs" role="alert"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+
+                <div class="relative pt-3">
+                    <label for="password" class="text-blue-500 text-xs font-bold pl-3 pt-2 absolute uppercase">Password</label>
+
+                    <input id="password" type="password" class="w-full p-3 pt-8 rounded focus:outline-none focus:bg-blue-700 bg-blue-800 text-gray-100 @error('password') border border-red-600 @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+
+                    @error('password')
+                    <span class="text-red-600 text-xs" role="alert"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+
+                <div class="relative pt-3">
+                    <label for="password-confirm" class="text-blue-500 text-xs font-bold pl-3 pt-2 absolute uppercase">Re-enter Password</label>
+
+                    <input id="password-confirm" type="password" class="w-full p-3 pt-8 rounded focus:outline-none focus:bg-blue-700 bg-blue-800 text-gray-100" name="password_confirmation" required placeholder="Confirm">
+                </div>
+
+                <div class="pt-6">
+                    <button type="submit" class="w-full rounded py-2 px-3 uppercase text-left bg-gray-400 text-blue-800 text-gray-100 font-bold">Register</button>
+                </div>
+
+                <div class="pt-8 flex justify-between text-white text-sm font-bold">
+                    <a class="hover:text-blue-200" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                    <a class="hover:text-blue-200" href="{{ route('login') }}">Login</a>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
