a733c0fe3e81ea6b34c8f9892cb702ceb2490783
Romanti-Ezer Santos <romanti.santos@fatec.sp.gov.br>
2020-02-02 00:58:13 -0300

    Show and delete contact

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -35,9 +35,7 @@
                     <div>
                         Contacts
                     </div>
-                    <div class="rounded-full border border-gray-400 text-white bg-blue-400 w-10 h-10 flex justify-center items-center">
-                        VG
-                    </div>
+                    <UserCircle :name="user.name"/>
                 </div>
 
                 <div class="flex flex-col overflow-y-hidden flex-1">
@@ -49,6 +47,8 @@
 </template>
 
 <script>
+    import UserCircle from './UserCircle';
+
     export default {
         name: "App",
 
@@ -56,13 +56,21 @@
             'user'
         ],
 
-        mounted() {
+        components: {
+            UserCircle
+        },
+
+        created() {
             window.axios.interceptors.request.use(
                 (config) => {
-                    config.data = {
-                        ...config.data,
-                        api_token: this.user.api_token
-                    };
+                    if (config.method === 'get') {
+                        config.url = config.url + '?api_token=' + this.user.api_token
+                    } else {
+                        config.data = {
+                            ...config.data,
+                            api_token: this.user.api_token
+                        };
+                    }
 
                     return config;
                 }
